d0e93a25a3bfbfe0441ea63ec8481159beca5e27
Victor <victor.w.boyd@gmail.com>
2023-10-21 08:23:27 -0500

    finished basic logic for countdown

--- countdown.js
+++ countdown.js
@@ -1,6 +1,43 @@
 
 
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
+const hoursId = document.getElementById('hours');
+const minutesId = document.getElementById('minutes');
+const secondsId = document.getElementById('seconds');
+
+const start = document.getElementById('start-button');
+
+start.addEventListener('click', function() {
+    let hours = hoursId.value
+    let minutes = minutesId.value
+    let seconds = secondsId.value
+
+    let countDownInterval = setInterval(countDown, 1000)
+
+    function countDown(){
+    if (seconds > 0 ) {
+        seconds -= 1
+        secondsId.value = seconds
+    }
+    else if (seconds == 0 && minutes > 0) {
+        minutes -= 1
+        seconds = 59
+        secondsId.value = seconds
+        minutesId.value = minutes
+
+    }
+    else if (seconds == 0 && minutes == 0 && hours > 0) {
+        hours -= 1
+        minutes = 59
+        seconds = 59
+        secondsId.value = seconds
+        minutesId.value = minutes
+        hoursId.value = hours
+    }
+    else {
+        clearInterval(countDownInterval)
+    }
+}
+})
+
+
 
